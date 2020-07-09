359dd4012c74d017bc6533e133d06ae3bb65b4c5
BriceCG <briceainarivonyraharijaona@gmail.com>
2020-07-09 21:14:24 +0300

    add doc

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const compression = require('compression')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
+
 
 const cors = require('cors')
 require('dotenv').config()
@@ -16,6 +19,7 @@ app.use('',require('./controller/departement.controller'))
 app.use('',require('./controller/user.controller'))
 app.use('',require('./controller/auth.controller'))
 app.use('',require('./controller/presence.controller'))
+app.use('/api-doc',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 app.listen(PORT,()=>{
     console.log('Serveur connecte au http://localhost:'+PORT)
 })
